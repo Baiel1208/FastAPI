e27625314a98b6b9940f0e1695b1e5cb0142b0ff
Baiel1208 <baiel000005@gmail.com>
2024-02-03 17:10:34 +0600

    add router

--- app/hotels/models.py
+++ app/hotels/models.py
@@ -17,10 +17,10 @@ class Hotels(Base):
 class Rooms(Base):
     __tablename__ = "rooms"
     id = Column(Integer, primary_key=True, nullable=False)
-    hotel_id = Column(ForeignKey("hotels.id"))
+    hotel_id = Column(ForeignKey("hotels.id"), nullable=False)
     name = Column(String, nullable=False)
-    description = Column(String)
+    description = Column(String, nullable=False)
     price = Column(Integer, nullable=False)
-    services = Column(JSON)
+    services = Column(JSON, nullable=False)
     quantity = Column(Integer, nullable=False)
     image_id = Column(Integer)
