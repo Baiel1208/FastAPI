f975c4e8ec9e3b48bc279ca0ef9d8290a9a003d0
Baiel1208 <baiel000005@gmail.com>
2024-02-04 15:55:17 +0600

    оптимизация кода

--- app/bookings/router.py
+++ app/bookings/router.py
@@ -1,9 +1,11 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from sqlalchemy import select
 
 from app.bookings.models import Bookings
 from app.bookings.dao import BookingDAO
 from app.bookings.schamas import SBooking
+from app.users.dependencies import get_current_user
+from app.users.models import Users
 
 
 router = APIRouter(
@@ -13,7 +15,7 @@ router = APIRouter(
 
 
 @router.get("")
-async def get_bookings() -> list[SBooking]:
-    return await BookingDAO.find_all()
+async def get_bookings(user: Users = Depends(get_current_user)): #-> list[SBooking]:
+    return await BookingDAO.find_all(user_id=user.id)
 
         
