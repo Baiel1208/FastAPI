b408ed7a99c4c58e94d1c48b9933aeee188194b3
Baiel1208 <baiel000005@gmail.com>
2024-02-04 15:01:41 +0600

    add login user

--- app/users/auth.py
+++ app/users/auth.py
@@ -1,12 +1,34 @@
+from datetime import datetime, timedelta
 from passlib.context import CryptContext
+from jose import jwt
+from pydantic import EmailStr
 
+from app.users.dao import UserDAO
+from app.config import settings
 
-pdw_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
 def get_password_hash(password: str):
-    return pdw_context.hash(password)
+    return pwd_context.hash(password)
 
 
 def verify_password(plain_password, hashed_password) -> bool:
-    return pdw_context.verify(plain_password, hashed_password)
+    return pwd_context.verify(plain_password, hashed_password)
+
+
+def create_access_token(data: dict) -> str:
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=30)
+    to_encode.update({'exp': expire})
+    encoded_jwt = jwt.encode(
+        to_encode, settings.SECRET_KEY, algorithm=settings.ALRORITHM
+    )
+    return encoded_jwt
+
+
+async def authenticate_user(email: EmailStr, password: str):
+    user = await UserDAO.find_one_or_none(email=email)
+    if not user and not verify_password(password, user.password):
+        return None
+    return user
